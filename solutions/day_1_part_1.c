c5c99b50df4281a55250a522d637e5b0cc1f8c5d
Florian Boisen <florianboisen@hotmail.de>
2025-12-01 09:48:49 +0100

    Day 1

--- solutions/day_1_part_1.c
+++ solutions/day_1_part_1.c
@@ -3,13 +3,35 @@
 #include "aoc_util/array_list.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 
 uint64_t solve_day_1_part_1(const puzzle_input* puzzle_input) {
-    printf("\n");
-
+    uint8_t dial_position = 50;
+    uint64_t counter = 0;
     for (size_t i = 0; i < array_list_get_length(puzzle_input->lines); ++i) {
-        printf("%ld: %s\n", i, puzzle_input->lines[i]);
+        const char* line = puzzle_input->lines[i];
+        uint16_t step = atoi(line + 1);
+        if (line[0] == 'L') {
+            for (uint16_t j = 0; j < step; ++j) {
+                if (dial_position == 0) {
+                    dial_position = 99;
+                } else {
+                    --dial_position;
+                }
+            }
+        } else {
+            for (uint16_t j = 0; j < step; ++j) {
+                if (dial_position == 99) {
+                    dial_position = 0;
+                } else {
+                    ++dial_position;
+                }
+            }
+        }
+        if (dial_position == 0) {
+            ++counter;
+        }
     }
 
-    return 0;
+    return counter;
 }
--- solutions/day_1_part_2.c
+++ solutions/day_1_part_2.c
@@ -1,5 +1,39 @@
 #include "solution.h"
 
+#include "aoc_util/array_list.h"
+
+#include <stdlib.h>
+
 uint64_t solve_day_1_part_2(const puzzle_input* puzzle_input) {
-    return 0;
+    uint8_t dial_position = 50;
+    uint64_t counter = 0;
+    for (size_t i = 0; i < array_list_get_length(puzzle_input->lines); ++i) {
+        const char* line = puzzle_input->lines[i];
+        uint16_t step = atoi(line + 1);
+        if (line[0] == 'L') {
+            for (uint16_t j = 0; j < step; ++j) {
+                if (dial_position == 0) {
+                    dial_position = 99;
+                } else {
+                    --dial_position;
+                }
+                if (dial_position == 0) {
+                    ++counter;
+                }
+            }
+        } else {
+            for (uint16_t j = 0; j < step; ++j) {
+                if (dial_position == 99) {
+                    dial_position = 0;
+                } else {
+                    ++dial_position;
+                }
+                if (dial_position == 0) {
+                    ++counter;
+                }
+            }
+        }
+    }
+
+    return counter;
 }
