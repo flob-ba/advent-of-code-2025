296d721baf411364df62719eb27487e0a08974cb
Florian Boisen <florianboisen@hotmail.de>
2025-12-03 08:48:42 +0100

    Day 3

--- solutions/day_3_part_1.c
+++ solutions/day_3_part_1.c
@@ -1,5 +1,35 @@
 #include "solution.h"
 
+#include "aoc_util/array_list.h"
+#include "aoc_util/string.h"
+
+#include <stdio.h>
+
 uint64_t solve_day_3_part_1(const puzzle_input* puzzle_input) {
-    return 0;
+    uint64_t result = 0;
+    for (size_t i = 0; i < array_list_get_length(puzzle_input->lines); ++i) {
+        uint8_t* digits = create_array_list(uint8_t);
+        array_list_reserve(digits, string_get_length(puzzle_input->lines[i]));
+        for (size_t j = 0; j < string_get_length(puzzle_input->lines[i]); ++j) {
+            array_list_push(digits, puzzle_input->lines[i][j] - '0');
+        }
+
+        size_t first_digit_position;
+        uint8_t first_digit = 0;
+        for (size_t j = 0; j < array_list_get_length(digits) - 1; ++j) {
+            if (digits[j] > first_digit) {
+                first_digit = digits[j];
+                first_digit_position = j;
+            }
+        }
+
+        uint8_t second_digit = 0;
+        for (size_t j = first_digit_position + 1; j < array_list_get_length(digits); ++j) {
+            if (digits[j] > second_digit) {
+                second_digit = digits[j];
+            }
+        }
+        result += 10 * first_digit + second_digit;
+    }
+    return result;
 }
--- solutions/day_3_part_2.c
+++ solutions/day_3_part_2.c
@@ -1,5 +1,39 @@
 #include "solution.h"
 
+#include "aoc_util/array_list.h"
+#include "aoc_util/string.h"
+
+#include <stdio.h>
+
 uint64_t solve_day_3_part_2(const puzzle_input* puzzle_input) {
-    return 0;
+    uint64_t result = 0;
+    for (size_t i = 0; i < array_list_get_length(puzzle_input->lines); ++i) {
+        uint8_t* digits = create_array_list(uint8_t);
+        array_list_reserve(digits, string_get_length(puzzle_input->lines[i]));
+        for (size_t j = 0; j < string_get_length(puzzle_input->lines[i]); ++j) {
+            array_list_push(digits, puzzle_input->lines[i][j] - '0');
+        }
+
+        uint8_t voltage_digits[12] = {0};
+        size_t voltage_digits_positions[12] = {0};
+
+        for (size_t j = 0; j < 12; ++j) {
+            for (size_t k = voltage_digits_positions[j]; k < array_list_get_length(digits) - (11 - j); ++k) {
+                if (digits[k] > voltage_digits[j]) {
+                    voltage_digits[j] = digits[k];
+                    voltage_digits_positions[j] = k;
+                }
+            }
+            if (j < 11) {
+                voltage_digits_positions[j + 1] = voltage_digits_positions[j] + 1;
+            }
+        }
+
+        uint64_t voltage = 0;
+        for (size_t j = 0; j < 12; ++j) {
+            voltage = 10 * voltage + voltage_digits[j];
+        }
+        result += voltage;
+    }
+    return result;
 }
