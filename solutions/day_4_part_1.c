303a31341161659ec8e647ef84346b978525aab9
Florian Boisen <florianboisen@hotmail.de>
2025-12-04 09:44:04 +0100

    Day 4

--- solutions/day_4_part_1.c
+++ solutions/day_4_part_1.c
@@ -1,5 +1,47 @@
 #include "solution.h"
 
+#include "aoc_util/array_list.h"
+#include "aoc_util/string.h"
+
 uint64_t solve_day_4_part_1(const puzzle_input* puzzle_input) {
-    return 0;
+    uint64_t result = 0;
+    for (size_t i = 0; i < array_list_get_length(puzzle_input->lines); ++i) {
+        for (size_t j = 0; j < string_get_length(puzzle_input->lines[i]); ++j) {
+            if (puzzle_input->lines[i][j] == '.')
+                continue;
+
+            uint8_t adjacent_rolls_count = 0;
+
+            bool has_space_above = i > 0;
+            bool has_space_below = i < array_list_get_length(puzzle_input->lines) - 1;
+            bool has_space_left = j > 0;
+            bool has_space_right = j < string_get_length(puzzle_input->lines[i]) - 1;
+
+            if (has_space_above) {
+                if (has_space_left && puzzle_input->lines[i - 1][j - 1] == '@')
+                    ++adjacent_rolls_count;
+                if (puzzle_input->lines[i - 1][j] == '@')
+                    ++adjacent_rolls_count;
+                if (has_space_right && puzzle_input->lines[i - 1][j + 1] == '@')
+                    ++adjacent_rolls_count;
+            }
+            if (has_space_left && puzzle_input->lines[i][j - 1] == '@')
+                ++adjacent_rolls_count;
+            if (has_space_right && puzzle_input->lines[i][j + 1] == '@')
+                ++adjacent_rolls_count;
+            if (has_space_below) {
+                if (has_space_left && puzzle_input->lines[i + 1][j - 1] == '@')
+                    ++adjacent_rolls_count;
+                if (puzzle_input->lines[i + 1][j] == '@')
+                    ++adjacent_rolls_count;
+                if (has_space_right && puzzle_input->lines[i + 1][j + 1] == '@')
+                    ++adjacent_rolls_count;
+            }
+
+            if (adjacent_rolls_count < 4) {
+                ++result;
+            }
+        }
+    }
+    return result;
 }
--- solutions/day_4_part_2.c
+++ solutions/day_4_part_2.c
@@ -1,5 +1,72 @@
 #include "solution.h"
 
+#include "aoc_util/array_list.h"
+#include "aoc_util/string.h"
+
+#include <stdlib.h>
+#include <string.h>
+
 uint64_t solve_day_4_part_2(const puzzle_input* puzzle_input) {
-    return 0;
+    size_t row_count = array_list_get_length(puzzle_input->lines);
+    size_t column_count = string_get_length(puzzle_input->lines[0]);
+
+    char** rolls = malloc(column_count * sizeof(char*));
+    for (size_t i = 0; i < row_count; ++i) {
+        rolls[i] = malloc(row_count);
+        memcpy(&rolls[i][0], puzzle_input->lines[i], column_count);
+    }
+
+    uint64_t result = 0;
+    bool is_something_removed = true;
+
+    while (is_something_removed) {
+        is_something_removed = false;
+        for (size_t i = 0; i < row_count; ++i) {
+            for (size_t j = 0; j < column_count; ++j) {
+                if (rolls[i][j] == '.')
+                    continue;
+
+                uint8_t adjacent_rolls_count = 0;
+
+                bool has_space_above = i > 0;
+                bool has_space_below = i < row_count - 1;
+                bool has_space_left = j > 0;
+                bool has_space_right = j < column_count - 1;
+
+                if (has_space_above) {
+                    if (has_space_left && rolls[i - 1][j - 1] == '@')
+                        ++adjacent_rolls_count;
+                    if (rolls[i - 1][j] == '@')
+                        ++adjacent_rolls_count;
+                    if (has_space_right && rolls[i - 1][j + 1] == '@')
+                        ++adjacent_rolls_count;
+                }
+                if (has_space_left && rolls[i][j - 1] == '@')
+                    ++adjacent_rolls_count;
+                if (has_space_right && rolls[i][j + 1] == '@')
+                    ++adjacent_rolls_count;
+                if (has_space_below) {
+                    if (has_space_left && rolls[i + 1][j - 1] == '@')
+                        ++adjacent_rolls_count;
+                    if (rolls[i + 1][j] == '@')
+                        ++adjacent_rolls_count;
+                    if (has_space_right && rolls[i + 1][j + 1] == '@')
+                        ++adjacent_rolls_count;
+                }
+
+                if (adjacent_rolls_count < 4) {
+                    rolls[i][j] = '.';
+                    ++result;
+                    is_something_removed = true;
+                }
+            }
+        }
+    }
+
+    for (size_t i = 0; i < row_count; ++i) {
+        free(rolls[i]);
+    }
+    free(rolls);
+
+    return result;
 }
